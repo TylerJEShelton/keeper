691acbc6286c4890ddc11f4bc29de59877c70f4a
Tyler <tylershelton99@gmail.com>
2022-01-13 16:26:33 -0500

    Complete the first part of the Keeper App tutorial

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Note() {
+  return (
+    <div className='note'>
+      <h1>This is the Title for the Note</h1>
+      <p>This is the Content for the Note.</p>
+    </div>
+  );
+}
