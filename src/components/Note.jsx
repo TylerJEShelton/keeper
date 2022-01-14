36cb766e8598b559cb16280d72f3e39039ef8301
Tyler <tylershelton99@gmail.com>
2022-01-13 20:55:27 -0500

    Broke out the note component and implemented the ability to read notes from a file and iterate through them using map function. Keeper App part 2 completed.

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-export default function Note() {
+export default function Note(props) {
+  const { title, content } = props;
+
   return (
     <div className='note'>
-      <h1>This is the Title for the Note</h1>
-      <p>This is the Content for the Note.</p>
+      <h1>{title}</h1>
+      <p>{content}</p>
     </div>
   );
 }
